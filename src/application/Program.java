2b8a6d128d8df7886f6cd584fc253589e774bf27
Sirio Junior <sirioroberto1@hotmail.com>
2022-12-31 00:04:48 -0300

    DB utility class created and connection test done

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,33 @@
 package application;
 
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from user");
+
+            while (rs.next()) {
+                System.out.println(rs.getString("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
